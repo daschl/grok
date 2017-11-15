48b7a2918514fa5ca55b3e1a2a452025e88358db
Michael Nitschinger <michael@nitschinger.at>
2017-11-15 09:42:48 +0100

    add benchmarks

---
+++ benches/log.rs
@@ -0,0 +1,71 @@
+#![feature(test)]
+
+extern crate grok;
+extern crate test;
+
+use test::Bencher;
+use grok::Grok;
+
+#[bench]
+fn bench_log_match(b: &mut Bencher) {
+    let msg = "2016-09-19T18:19:00 [8.8.8.8:prd] DEBUG this is an example log message";
+
+    let mut grok = Grok::default();
+    let pattern = grok.compile(r"%{TIMESTAMP_ISO8601:timestamp} \[%{IPV4:ip}:%{WORD:environment}\] %{LOGLEVEL:log_level} %{GREEDYDATA:message}", false)
+        .expect("Error while compiling!");
+
+    b.iter(|| match pattern.match_against(msg) {
+        Some(found) => {
+            test::black_box(found);
+        }
+        None => (),
+    });
+}
+
+#[bench]
+fn bench_log_no_match(b: &mut Bencher) {
+    let msg = "2016-09-19T18:19:00 [8.8.8.8:prd] DEBUG this is an example log message";
+
+    let mut grok = Grok::default();
+    let pattern = grok.compile(r"%{TIMESTAMP_ISO8601:timestamp} \[%{IPV4:ip};%{WORD:environment}\] %{LOGLEVEL:log_level} %{GREEDYDATA:message}", false)
+        .expect("Error while compiling!");
+
+    b.iter(|| match pattern.match_against(msg) {
+        Some(found) => {
+            test::black_box(found);
+        }
+        None => (),
+    });
+}
+
+#[bench]
+fn bench_log_match_with_anchors(b: &mut Bencher) {
+    let msg = "2016-09-19T18:19:00 [8.8.8.8:prd] DEBUG this is an example log message";
+
+    let mut grok = Grok::default();
+    let pattern = grok.compile(r"^%{TIMESTAMP_ISO8601:timestamp} \[%{IPV4:ip}:%{WORD:environment}\] %{LOGLEVEL:log_level} %{GREEDYDATA:message}$", false)
+        .expect("Error while compiling!");
+
+    b.iter(|| match pattern.match_against(msg) {
+        Some(found) => {
+            test::black_box(found);
+        }
+        None => (),
+    });
+}
+
+#[bench]
+fn bench_log_no_match_with_anchors(b: &mut Bencher) {
+    let msg = "2016-09-19T18:19:00 [8.8.8.8;prd] DEBUG this is an example log message";
+
+    let mut grok = Grok::default();
+    let pattern = grok.compile(r"^%{TIMESTAMP_ISO8601:timestamp} \[%{IPV4:ip}:%{WORD:environment}\] %{LOGLEVEL:log_level} %{GREEDYDATA:message}$", false)
+        .expect("Error while compiling!");
+
+    b.iter(|| match pattern.match_against(msg) {
+        Some(found) => {
+            test::black_box(found);
+        }
+        None => (),
+    });
+}
---
+++ benches/simple.rs
@@ -0,0 +1,67 @@
+#![feature(test)]
+
+extern crate grok;
+extern crate test;
+
+use test::Bencher;
+use grok::Grok;
+
+#[bench]
+fn bench_simple_pattern_match(b: &mut Bencher) {
+    let mut grok = Grok::empty();
+    grok.insert_definition("USERNAME", r"[a-zA-Z0-9._-]+");
+    let pattern = grok.compile("%{USERNAME}", false)
+        .expect("Error while compiling!");
+
+    b.iter(|| match pattern.match_against("user") {
+        Some(found) => {
+            test::black_box(found);
+        }
+        None => (),
+    });
+}
+
+#[bench]
+fn bench_simple_pattern_no_match(b: &mut Bencher) {
+    let mut grok = Grok::empty();
+    grok.insert_definition("USERNAME", r"[a-zA-Z0-9._-]+");
+    let pattern = grok.compile("%{USERNAME}", false)
+        .expect("Error while compiling!");
+
+    b.iter(|| match pattern.match_against("$$$$") {
+        Some(found) => {
+            test::black_box(found);
+        }
+        None => (),
+    });
+}
+
+#[bench]
+fn bench_simple_pattern_match_with_anchor(b: &mut Bencher) {
+    let mut grok = Grok::empty();
+    grok.insert_definition("USERNAME", r"[a-zA-Z0-9._-]+");
+    let pattern = grok.compile("^%{USERNAME}$", false)
+        .expect("Error while compiling!");
+
+    b.iter(|| match pattern.match_against("user") {
+        Some(found) => {
+            test::black_box(found);
+        }
+        None => (),
+    });
+}
+
+#[bench]
+fn bench_simple_pattern_no_match_with_anchor(b: &mut Bencher) {
+    let mut grok = Grok::empty();
+    grok.insert_definition("USERNAME", r"[a-zA-Z0-9._-]+");
+    let pattern = grok.compile("^%{USERNAME}$", false)
+        .expect("Error while compiling!");
+
+    b.iter(|| match pattern.match_against("$$$$") {
+        Some(found) => {
+            test::black_box(found);
+        }
+        None => (),
+    });
+}
